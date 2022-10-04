600cffaa5eaf0fbd87bba9ce3d868faa9e704633
AlanGPT-93 <argarcia1693@gmail.com>
2022-10-03 21:08:57 -0500

    We made a template

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from glob import escape
 from urllib import response
-from flask import Flask, make_response, redirect, request
+from flask import Flask, make_response, redirect, request, render_template
 
 
 app = Flask(__name__)
 
+to_dos = ["do1", "do2", "do3"]
+
 @app.route("/")
 def index():
     #js = "<style> * {background: #00ff00} </style>"  #
-    js = request.remote_addr
+    user_ip = request.remote_addr
     response = make_response(redirect("/hello"))
-    response.set_cookie("js", js)
+    response.set_cookie("user_ip", user_ip)
     return response
 
 @app.route("/hello")
 def hello():
-    js = request.cookies.get('js')
-    js = escape(js)
-    return f"hole {js}"
+    user_ip = request.cookies.get('user_ip')
+    #user_ip = escape(user_ip)
+    context = {
+        "to_dos": to_dos,
+        "user_ip": user_ip
+    }
+
+    return render_template("hello.html", **context)
 
 if __name__ == "__main__":
     app.run(port = 8080, debug = True)
