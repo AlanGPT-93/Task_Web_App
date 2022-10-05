0c315d458664d4176791000408a511d954a765f0
AlanGPT-93 <argarcia1693@gmail.com>
2022-10-04 23:11:35 -0500

    Using WTFs library

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from ensurepip import bootstrap
-from urllib import response
 from flask import Flask, make_response, redirect, request, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
-app = Flask(__name__,
-static_folder =  "static")
+
+app = Flask(__name__, static_folder =  "static")
 app.config.update(ENV = "development")
 app.config["SECRET_KEY"] = "TOP SECRET"
+
+class LoginForm(FlaskForm):
+    username = StringField("User Name", validators = [DataRequired()]) 
+    password = PasswordField("Password", validators = [DataRequired()])
+    submit = SubmitField("Enviar")
 
 bootstrap = Bootstrap(app)
 
@@ -26,9 +32,12 @@ def index():
 def hello():
     #user_ip = request.cookies.get('user_ip')
     user_ip = session.get("user_ip")
+    login_form = LoginForm()
+
     context = {
         "to_dos": to_dos,
-        "user_ip": user_ip
+        "user_ip": user_ip,
+        "login_form": login_form
     }
 
     return render_template("hello.html", **context)
